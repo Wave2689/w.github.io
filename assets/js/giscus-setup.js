aae64175472aa66a342156391eb0a27b936044c6
Wave2689 <60916607+Wave2689@users.noreply.github.com>
2026-05-12 05:07:12 +0000

    Deploying to gh-pages from @ Wave2689/botao.github.io@3684f21890b5335d51adc047f3dac7149205999e 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Wave2689/botao",
+    "data-repo": "Wave2689/botao.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
